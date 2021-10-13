db07c565d4f57421191adc2d40adb5da6602c2c9
Tetrergeru <41305740+Tetrergeru@users.noreply.github.com>
2021-10-13 23:05:58 +0300

    Add todo elements

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,10 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   const state = getStoredStateOrDefault({
     counter: 40
   })
 
   const $incrButton = document.querySelector('.incr')!
   const $decrButton = document.querySelector('.decr')!
+
 
   const $gauge = document.querySelector('.gauge')! as HTMLDivElement
   setGaugePercent($gauge, state.counter)
@@ -20,4 +21,38 @@ document.addEventListener('DOMContentLoaded', function () {
     saveState(state)
     setGaugePercent($gauge, state.counter)
   })
+
+  const createButton = document.querySelector('.create-task')!
+  const taskList = document.querySelector('.task-container')! as HTMLDivElement
+  createButton.addEventListener('click', () => {
+    let deleteButton = createDiv(["delete-button"])
+    
+    const task = createDiv(['task'], [
+      createDiv(['checkbox']),
+      createDiv(['text'], "Make sfedu great again"),
+      deleteButton,
+    ]);
+
+    const clickHandler = () => {
+      task.remove()
+      deleteButton.removeEventListener('click', clickHandler)
+    }
+    deleteButton.addEventListener('click', clickHandler)
+
+    taskList.appendChild(task)
+  })
 })
+
+function createDiv(styles: string[] = [], content: HTMLElement[] | string = ""): HTMLElement {
+  const div = document.createElement('div')
+  for (const style of styles)
+    div.classList.add(style)
+  if (typeof (content) === 'string') {
+    div.innerText = content
+  } else {
+    for (const child of content){
+      div.appendChild(child)
+    }
+  }
+  return div
+}
